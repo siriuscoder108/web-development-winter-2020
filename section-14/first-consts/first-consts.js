15f5e42b60a7a3ba6530eac34839044fe20414ab
Karina Gonzalez <siriuscoder108@yahoo.com>
2020-10-23 19:17:34 -0700

    Add section 14 const and variables exercises

---
+++ section-14/first-consts/first-consts.js
@@ -0,0 +1,5 @@
+const boilingPointC = 100;
+const boilingPointF = 212;
+
+console.log("boilingPointC is " + boilingPointC);
+console.log("boilingPointF is " + boilingPointF);
---
+++ section-14/first-variables/first-variables.js
@@ -0,0 +1,5 @@
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+console.log("myLuckyNumber is " + myLuckyNumber);
+console.log("octopusLimbs is " + octopusLimbs);
