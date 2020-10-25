887b0a9301c8680f7faa08d808f9b18b2eab5109
Karina Gonzalez <siriuscoder108@yahoo.com>
2020-10-24 21:22:46 -0700

    Add section 19 for loop exercises

---
+++ section-19/31-for-loops/31-for-loops.js
@@ -0,0 +1,5 @@
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+
+for (let i = 0; i < 6; i++) {
+    console.log("Da ba dee da ba daa");
+}
---
+++ section-19/33-iterating-arrays/33-iterating-arrays.js
@@ -0,0 +1,7 @@
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+const len = people.length;
+for (let i = 0; i < len ; i++) {
+    console.log(people[i].toUpperCase());
+}
